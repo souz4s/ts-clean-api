9e423644d24278acbc4e2aa487cf9b0980cd963e
souz4s <psouza.1650@gmail.com>
2022-07-26 18:42:58 -0300

    feat: jest setup

---
+++ .jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.+)": "<rootDir>/src/$1",
+  },
+};
