2451ed0d1a8921909ad53fb5511ee2f1082fd64b
souz4s <psouza.1650@gmail.com>
2022-07-31 02:19:20 -0300

    perf: just email and name used as parameters in domain use case to create user

--- src/domain/use-cases/create-user.ts
+++ src/domain/use-cases/create-user.ts
@@ -1,17 +1,9 @@
-import { MusicalGenreModel } from "@/domain/models";
-
 export interface CreateUser {
   perform: (params: CreateUser.Params) => Promise<CreateUser.Result>;
 }
 
 export namespace CreateUser {
-  export type Params = {
-    id: string;
-    email: string;
-    name: string;
-    musicalGenre: MusicalGenreModel["name"];
-    musicalGenresId: string;
-  };
+  export type Params = { email: string; name: string };
 
   export type Result = { id: string };
 }
