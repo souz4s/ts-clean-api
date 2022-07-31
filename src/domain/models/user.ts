669a0c2d45377a69081018fb7d3a891aa2a19c1d
souz4s <psouza.1650@gmail.com>
2022-07-31 01:29:37 -0300

    feat: user model of domain

---
+++ src/domain/models/user.ts
@@ -0,0 +1,9 @@
+import { MusicalGenreModel } from "./musical-genre";
+
+export type UserModel = {
+  id: string;
+  email: string;
+  name: string;
+  musicalGenre: MusicalGenreModel;
+  musicalGenresId: string;
+};
