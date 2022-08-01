5e4201878c1d9088728ba172c5ce2d430655dd0e
souz4s <psouza.1650@gmail.com>
2022-08-01 00:33:08 -0300

    feat: user repository with create user and get by email functions

---
+++ src/infrastructure/db/repositories/user-repository.ts
@@ -0,0 +1,28 @@
+import { CreateUserRepository, GetUserByEmailRepository } from "@/data/protocols";
+import { prismaClient } from "@/infrastructure/db";
+
+export class UserRepository implements CreateUserRepository, GetUserByEmailRepository {
+  createUser = async (params: CreateUserRepository.Params): Promise<CreateUserRepository.Result> => {
+    const userCollection = prismaClient.getConnection().users;
+    const user = await userCollection.create({
+      data: {
+        ...params,
+        musicalGenre: {},
+        musicalGenreId: undefined,
+      },
+    });
+
+    return user;
+  };
+
+  getByEmail = async (params: GetUserByEmailRepository.Params): Promise<GetUserByEmailRepository.Result> => {
+    const userCollection = prismaClient.getConnection().users;
+    const user = await userCollection.findFirst({
+      where: {
+        email: params.userEmail,
+      },
+    });
+
+    return { user: user || undefined };
+  };
+}
