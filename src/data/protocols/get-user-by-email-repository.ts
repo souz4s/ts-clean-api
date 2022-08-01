bc5e766925e866f236879f2aacc0e5045bc25ced
souz4s <psouza.1650@gmail.com>
2022-08-01 00:07:34 -0300

    refactor: user model removed from GetUserByEmailReposiory result

--- src/data/protocols/get-user-by-email-repository.ts
+++ src/data/protocols/get-user-by-email-repository.ts
@@ -1,10 +1,8 @@
-import { UserModel } from "@/domain/models";
-
 export interface GetUserByEmailRepository {
   getByEmail: (params: GetUserByEmailRepository.Params) => Promise<GetUserByEmailRepository.Result>;
 }
 
 export namespace GetUserByEmailRepository {
   export type Params = { userEmail: string };
-  export type Result = { user?: UserModel };
+  export type Result = { user?: object };
 }
