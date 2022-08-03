17e66c755045d291c4ca2ee578db5454edd40f93
souz4s <psouza.1650@gmail.com>
2022-08-03 20:02:56 -0300

    refactor: result const changed to createResult

--- src/data/use-cases/get-user-by-email/db-get-user-by-email.ts
+++ src/data/use-cases/get-user-by-email/db-get-user-by-email.ts
@@ -4,10 +4,10 @@ import { GetUserByEmailRepository } from "@/data/protocols";
 export class DbGetUserByEmail implements GetUserByEmail {
   constructor(private readonly getUserByEmailRepository: GetUserByEmailRepository) {}
   perform = async (params: GetUserByEmail.Params): Promise<GetUserByEmail.Result> => {
-    const result = await this.getUserByEmailRepository.getByEmail({
+    const createResult = await this.getUserByEmailRepository.getByEmail({
       email: params.email,
     });
 
-    return { user: result.user };
+    return { user: createResult.user };
   };
 }
--- src/data/use-cases/update-musical-genre-score/db-update-musical-genre-score.ts
+++ src/data/use-cases/update-musical-genre-score/db-update-musical-genre-score.ts
@@ -4,10 +4,10 @@ import { UpdateMusicalGenreScoreRepository } from "@/data/protocols";
 export class DbUpdateMusicalGenreScore implements UpdateMusicalGenreScore {
   constructor(private readonly updateMusicalGenreScoreRepository: UpdateMusicalGenreScoreRepository) {}
   perform = async (params: UpdateMusicalGenreScore.Params): Promise<UpdateMusicalGenreScore.Result> => {
-    const result = await this.updateMusicalGenreScoreRepository.updateScore({
+    const createResult = await this.updateMusicalGenreScoreRepository.updateScore({
       id: params.id,
     });
 
-    return { score: result.score };
+    return { score: createResult.score };
   };
 }
