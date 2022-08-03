e117cd18a6539ad80ebad93a78ca09089c116f1f
souz4s <psouza.1650@gmail.com>
2022-08-03 00:39:44 -0300

    feat: http helper added in interface adapters

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,40 @@
+import { InternalServerError } from "@/presentation/errors";
+import { HttpResponse } from "@/presentation/protocols";
+
+export class HttpHelper {
+  static BAD_REQUEST = (err: Error): HttpResponse<Error> => ({
+    statusCode: 400,
+    body: err,
+  });
+
+  static FORBIDDEN = (err: Error): HttpResponse<Error> => ({
+    statusCode: 403,
+    body: err,
+  });
+
+  static NOT_FOUND = (err: Error): HttpResponse<Error> => ({
+    statusCode: 404,
+    body: err,
+  });
+
+  static INTERNAL_SERVER_ERROR = (err: Error): HttpResponse<Error> => ({
+    statusCode: 500,
+    body: new InternalServerError(err.stack || ""),
+  });
+
+  static OK = <T>(data: T, message?: string): HttpResponse<T> => ({
+    statusCode: 200,
+    body: data,
+    message,
+  });
+
+  static CREATED = <T>(data: T): HttpResponse<T> => ({
+    statusCode: 201,
+    body: data,
+    message: "Successfully created",
+  });
+
+  static NOT_CONTENT = (): HttpResponse<null> => ({
+    statusCode: 204,
+  });
+}
