f1849e8688b148250d9dabccbc2ca8439d968662
souz4s <psouza.1650@gmail.com>
2022-08-03 20:44:29 -0300

    test: ensure call 'createUser' in the creation of the user

---
+++ src/presentation/controllers/create-user-controller/create-user-controller.spec.ts
@@ -0,0 +1,47 @@
+import { CreateUserController } from ".";
+import { UserModel } from "@/domain/models";
+import { CreateUser } from "@/domain/use-cases";
+import { OnCreateController } from "@/presentation/protocols";
+
+class CreateUserSpy implements CreateUser {
+  callsCount = 0;
+  result: CreateUser.Result = {
+    id: 0,
+  };
+  perform = async (params: CreateUser.Params): Promise<CreateUser.Result> => {
+    void (await Promise.resolve(params));
+    this.callsCount++;
+
+    return { id: this.result.id };
+  };
+}
+
+const mockUserModel = (overwriteModel: Partial<UserModel> = {}): UserModel => ({
+  id: 0,
+  email: "some-email",
+  name: "some-name",
+  ...overwriteModel,
+});
+
+const mockParams = (user: UserModel): OnCreateController.Params<UserModel> => ({
+  eventType: "created",
+  previousData: undefined,
+  newData: user,
+});
+
+const makeSut = () => {
+  const createUserSpy = new CreateUserSpy();
+  const sut = new CreateUserController(createUserSpy);
+
+  return { sut, createUserSpy };
+};
+
+describe("CreateUserController", () => {
+  it("should call 'createUser' in the creation of the user", async () => {
+    const params = mockParams(mockUserModel());
+    const { sut, createUserSpy } = makeSut();
+    await sut.handle(params);
+
+    expect(createUserSpy.callsCount).toBe(1);
+  });
+});
