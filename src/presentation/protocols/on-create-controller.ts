1f0e1031ceb695a4f1454d342e4875f94f87dc46
souz4s <psouza.1650@gmail.com>
2022-08-04 22:36:14 -0300

    perf: on create and update controller removed

--- src/presentation/protocols/on-create-controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@/presentation/protocols";
-
-export interface OnCreateController<T = unknown> extends Controller {
-  handle: (params: OnCreateController.Params<T>) => Promise<unknown>;
-}
-
-export declare namespace OnCreateController {
-  export type Params<T = unknown> = { eventType: "created"; previousData: undefined; newData: T };
-}
--- src/presentation/protocols/on-update-controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@/presentation/protocols";
-
-export interface OnUpdateController<T = unknown> extends Controller {
-  handle: (params: OnUpdateController.Params<T>) => Promise<unknown>;
-}
-
-export declare namespace OnUpdateController {
-  export type Params<T = unknown> = { eventType: "updated"; previousData: T; newData: T };
-}
