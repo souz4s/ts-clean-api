651981f3c7738e81c14b51240fa7591af44ed4ad
souz4s <psouza.1650@gmail.com>
2022-08-07 15:37:00 -0300

    feat: express router adapter

---
+++ src/main/adapters/express-router-adapter.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import type { Request, Response } from "express";
+
+import type { Controller } from "@/presentation/protocols";
+
+export const expressRouterAdapter = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {}),
+    };
+
+    const httpResponse = await controller.handle(request);
+
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        ...httpResponse.body,
+        message: httpResponse.body as string,
+      });
+    }
+  };
+};
