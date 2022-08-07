ebeff53541d2b67fec7d2297a560a5de5d8a2869
souz4s <psouza.1650@gmail.com>
2022-08-07 16:08:17 -0300

    feat: server config in main

---
+++ src/main/server.ts
@@ -0,0 +1,12 @@
+import { prismaClient } from "@/infrastructure/db";
+import { App, Env } from "@/main/config";
+
+prismaClient
+  .createConnection()
+  .then(async () => {
+    void (await Promise.resolve());
+    const app = App();
+
+    app.listen(Env.PORT, () => console.log(`\n> Server is running on port ${Env.PORT}`));
+  })
+  .catch((err) => console.log(err));
