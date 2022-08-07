da306657a60d4b99d00790259fadd156502ceba4
souz4s <psouza.1650@gmail.com>
2022-08-07 16:01:32 -0300

    feat: app config in main

---
+++ src/main/config/app.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express from "express";
+
+import { router } from "@/main/config";
+
+export const App = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(router);
+
+  return app;
+};
