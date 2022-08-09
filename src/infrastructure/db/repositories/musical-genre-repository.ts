293d9a9886341ccef949365ca505f23a4c4eb5b7
souz4s <psouza.1650@gmail.com>
2022-08-08 22:59:25 -0300

    fix: getting current score to update

--- src/infrastructure/db/repositories/musical-genre-repository.ts
+++ src/infrastructure/db/repositories/musical-genre-repository.ts
@@ -4,12 +4,18 @@ import { prismaClient } from "@/infrastructure/db";
 export class MusicalGenreRepository implements UpdateMusicalGenreScoreRepository {
   updateScore = async (params: UpdateMusicalGenreScoreRepository.Params): Promise<UpdateMusicalGenreScoreRepository.Result> => {
     const musicalGenreCollection = prismaClient.getConnection().musicalGenres;
+    const getMusicalGenreScore = await musicalGenreCollection.findFirst({
+      where: {
+        id: params.id,
+      },
+    });
+    const score = getMusicalGenreScore?.score != undefined ? getMusicalGenreScore?.score + 1 : 0;
     const musicalGenre = await musicalGenreCollection.update({
       where: {
         id: params.id,
       },
       data: {
-        score: +1,
+        score: score,
       },
     });
 
