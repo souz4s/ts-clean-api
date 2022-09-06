75de54cced3c08848d151197a369b1b2419667b8
souz4s <psouza.1650@gmail.com>
2022-09-06 02:01:35 -0300

    test: ensure call 'updateMusicalGenreScoreRepository'

--- tests/data/use-cases/db-update-musical-genre-score.spec.ts
+++ tests/data/use-cases/db-update-musical-genre-score.spec.ts
@@ -1,29 +1,18 @@
-import { DbUpdateMusicalGenreScore } from ".";
-import { UpdateMusicalGenreScoreRepository } from "@/data/protocols";
+import { DbUpdateMusicalGenreScore } from "@/data/use-cases";
+import { UpdateMusicalGenreScoreRepositorySpy } from "@/tests/data/mocks";
 
-class UpdateMusicalGenreScoreRepositorySpy implements UpdateMusicalGenreScoreRepository {
-  result = 1;
-  callsCount = 0;
-  updateScore = async (params: UpdateMusicalGenreScoreRepository.Params): Promise<UpdateMusicalGenreScoreRepository.Result> => {
-    void (await Promise.resolve(params));
-    this.callsCount++;
-
-    return { score: this.result };
-  };
-}
+import { faker } from "@faker-js/faker";
 
 const makeSut = () => {
   const updateMusicalGenreScoreRepositorySpy = new UpdateMusicalGenreScoreRepositorySpy();
   const sut = new DbUpdateMusicalGenreScore(updateMusicalGenreScoreRepositorySpy);
-
   return { sut, updateMusicalGenreScoreRepositorySpy };
 };
 
 describe("DbUpdateMusicalGenreScore", () => {
   it("should call 'updateMusicalGenreScoreRepository'", async () => {
     const { sut, updateMusicalGenreScoreRepositorySpy } = makeSut();
-    const result = await sut.perform({ id: 0 });
-
+    const result = await sut.perform({ id: parseInt(faker.random.numeric(1)) });
     expect(result).toHaveProperty("score");
     expect(updateMusicalGenreScoreRepositorySpy.callsCount).toBe(1);
   });
