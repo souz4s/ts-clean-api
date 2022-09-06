996d8286b4f923ec366c11115939955000765443
souz4s <psouza.1650@gmail.com>
2022-09-06 00:26:18 -0300

    chore: test folder setting in jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
   testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
   moduleNameMapper: {
+    "@/tests/(.*)": "<rootDir>/tests/$1",
     "@/(.+)": "<rootDir>/src/$1",
   },
 };
