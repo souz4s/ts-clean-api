9f4546ed6c35c53dcae260b51cfc4ee2c249c762
souz4s <p.uzah@outlook.com>
2022-09-14 10:36:28 -0300

    perf: deprecated error handling removed

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,3 +1,2 @@
 export * from "./internal-server-error";
-export * from "./invalid-parameters-error";
 export * from "./missing-parameters-error";
--- src/presentation/errors/invalid-parameters-error.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidParametersError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "Invalid Parameters Error";
-  }
-}
